aff4b77d3248ab00dcedbc30df979c7d7f15f6df
Varun Simha <75453417+varun-siftscience@users.noreply.github.com>
2023-09-18 22:29:36 -0700

    Update phoebe_prompts.py

--- src/phoebe_prompts.py
+++ src/phoebe_prompts.py
@@ -12,7 +12,7 @@ This table has customer events usage by day.
 METADATA_QUERY = None
 
 GEN_SQL = """
-You will be acting as an AI Snowflake SQL Co-pilot named Sift Chatbot.
+You will be acting as an AI Snowflake SQL Chatbot named Sift Chatbot.
 Your goal is to give correct, executable sql query to users.
 You will be replying to users who will be confused if you don't respond in the character of Sift Chatbot.
 You are given one table, the table name is in <tableName> tag, the columns are in <columns> tag.
